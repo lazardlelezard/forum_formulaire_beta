32af9fa5c60bacd6b242f2b5835ca32388bca908
lazard612 <117316285+lazardlelezard@users.noreply.github.com>
2022-11-25 10:03:45 +0100

    Add files via upload

---
+++ formulaire/script.js
@@ -0,0 +1,49 @@
+let buttoninscription = document.querySelector('button')
+let input = document.querySelector('input')
+let h2 = document.querySelector('h2')
+let formulaire = document.querySelector('#formulaire')
+
+
+
+
+buttoninscription.addEventListener('click' , function(){
+	console.log('ta cliquer 😎')
+	
+})
+
+
+//element.addEventListener("animationstart", listener, false);
+//element.addEventListener("animationend", listener, false);
+//element.addEventListener("animationiteration", listener, false);
+
+//element.className = "slidein";
+
+
+
+
+formulaire.addEventListener('mouseout' ,  function(){
+	
+	
+})
+
+formulaire.addEventListener('mouseover' , function(){
+	
+
+})
+
+
+input.addEventListener('keydown' , (event) => {
+	console.log(event)
+})
+
+
+
+
+
+
+
+
+
+
+
+
